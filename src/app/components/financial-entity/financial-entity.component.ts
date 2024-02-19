f67b019c747cbb5e094e3f7050281c4554020ff1
trikytrukos <miguelmartinezsantos2003@gmail.com>
2024-02-19 20:33:35 +0100

    re-added user components

---
+++ src/app/components/financial-entity/financial-entity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-entity',
+  standalone: true,
+  imports: [],
+  templateUrl: './financial-entity.component.html',
+  styleUrl: './financial-entity.component.css'
+})
+export class FinancialEntityComponent {
+
+}
