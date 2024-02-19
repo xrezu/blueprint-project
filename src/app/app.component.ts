c5cf1b98b93d0c4d924cbf3882dc22f5d5b0a949
trikytrukos <miguelmartinezsantos2003@gmail.com>
2024-02-19 17:30:45 +0100

    adding component main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from "./components/main/main.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MainComponent, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'blueprint-project';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';  
 import { AdminComponent } from './admin/admin.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PromoterComponent } from './promoter/promoter.component';
+import { CitizenComponent } from './citizen/citizen.component';
+import { FinantialEntityComponent } from './finantial-entity/finantial-entity.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'user', component: PromoterComponent },
+  { path: 'user', component: CitizenComponent },
+  { path: 'user', component: FinantialEntityComponent },
+
+
   
   // Redirige a login por defecto
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // Agrega otras rutas aquí
 ];
 
+// ...
 @NgModule({
   
   declarations: [
+    
   ],
   imports: [
     AppComponent,
@@ -27,6 +35,10 @@ const routes: Routes = [
     FooterComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor() {
+    bootstrapApplication(AppComponent);
+  }
+}
