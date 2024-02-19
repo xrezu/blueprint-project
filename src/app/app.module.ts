ca50adac9afb3410258818d36d0015935869b50c
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-19 21:07:52 +0100

    Campios de las routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routes';
+import { ComplaintFormComponent } from './complaint-form/complaint-form.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
-import { LoginComponent } from './auth/login/login.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { PromoterComponent } from './components/promoter/promoter.component';
-import { CitizenComponent } from './components/citizen/citizen.component';
-import { FinancialEntityComponent } from './components/financial-entity/financial-entity.component';
-
-  const routes: Routes = [
-    // Nota: La propiedad 'imports' en las rutas es utilizada para componentes standalone en Angular 14+
-    { path: 'login', component: LoginComponent },
-    { path: 'admin', component: AdminComponent },
-    // Asegúrate de tener rutas únicas para cada tipo de usuario
-    { path: 'promoter', component: PromoterComponent },
-    { path: 'citizen', component: CitizenComponent },
-    { path: 'financial-entity', component: FinancialEntityComponent },
-    
-    // Redirige a login por defecto
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-  ];
-
-  @NgModule({
-    imports: [
-      BrowserModule,
-      RouterModule.forRoot(routes), // Configura tus rutas aquí
-    ],
-    bootstrap: [AppComponent] // Bootstrap con AppComponent
-  })
-  export class AppModule { }
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComplaintFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppComponent
+  ],
+  providers: []
+})
+export class AppModule {}
