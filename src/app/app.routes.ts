6371899fcf1cccac69cc8722025d67d1a42a56f0
trikytrukos <miguelmartinezsantos2003@gmail.com>
2024-02-19 20:41:20 +0100

    added angular-material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
