56e59da2a94a71c8bb7f5506367153294ada7a7e
Maksym Dovgan <maxdovgan959@gmail.com>
2024-02-19 17:21:37 +0100

    Add footer al proyecto principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'blueprint-project';
 }
+
