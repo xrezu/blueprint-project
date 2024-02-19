663f2fc9ad63f7673c26f05414e5dd37f02278d0
Maksym Dovgan <maxdovgan959@gmail.com>
2024-02-19 17:39:59 +0100

    Create contact form component

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.css'
+})
+export class ContactFormComponent {
+
+}
