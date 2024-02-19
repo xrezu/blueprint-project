1c20b84682b6cf6ab9ff2b707e4fadc71fb40a8d
trikytrukos <miguelmartinezsantos2003@gmail.com>
2024-02-19 17:00:44 +0100

    minor fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';  
 import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { PromoterComponent } from './promoter/promoter.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'user', component: UserComponent },
+  { path: 'user', component: PromoterComponent },
+  
   // Redirige a login por defecto
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // Agrega otras rutas aquí
