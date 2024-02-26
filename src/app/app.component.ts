9ff435ad49bef352cd36b69df9c485a12caa6ec5
at6DAW2 <at6DAW2@informatica.iesjc>
2024-02-26 17:26:43 +0100

    subiendo estructura html terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { HeaderComponent } from './shared/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CitizenComponent } from './components/citizen/citizen.component';
+import { PromoterComponent } from './components/promoter/promoter.component';
+import { FinancialEntityComponent } from './components/financial-entity/financial-entity.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +14,10 @@ import { CitizenComponent } from './components/citizen/citizen.component';
     HeaderComponent,
     MainComponent,
     FooterComponent,
-    CitizenComponent
+    CitizenComponent,
+    PromoterComponent,
+    FinancialEntityComponent,
+    AdminComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
