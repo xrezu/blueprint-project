d8bb3ad629b756bf83164387804563405ed9058b
alvarobarasona <dawjdc2022@gmail.com>
2024-02-27 17:02:41 +0100

    subiendo estilos de usuarios terminados

--- src/app/components/financial-entity/financial-entity.component.ts
+++ src/app/components/financial-entity/financial-entity.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataService } from '../../services/data.service';
+import { UserContribution } from '../../models/contributions.interface';
+import { Promoter } from '../../models/promoter.model';
+import { FinancialEntity } from '../../models/FEntity.model';
+import { User } from '../../models/user.model';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-financial-entity',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './financial-entity.component.html',
   styleUrl: './financial-entity.component.css'
 })
-export class FinancialEntityComponent {
+export class FinancialEntityComponent implements OnInit {
+  contributions: UserContribution[] = [];
+  financialEntity: { financialEntityId: string, name: string, contactEmail: string; }[] | undefined;
 
+  constructor(private dataServive: DataService) {}
+
+  ngOnInit() {
+    this.loadContributions();
+  }
+
+  loadContributions(): void {
+  
+  }
 }
