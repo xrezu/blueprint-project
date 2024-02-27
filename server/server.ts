b1405771ad0da4091b336d81d067d5120293cfcd
Maksym Dovgan <maxdovgan959@gmail.com>
2024-02-27 17:08:38 +0100

    Rectificadas rutas y el login post

--- server/server.ts
+++ server/server.ts
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Ruta absoluta del archivo de usuarios
-const usersFileJSON = 'server/src/assets/json/users.json';
+const usersFileJSON = path.resolve(__dirname, 'src/assets/json/users.json');
 
 // Ruta absoluta del archivo de reclamaciones, ajustar según la ubicación real
-const claimsFilePath = path.resolve(__dirname, '../../assets/json/claims.json');
+const claimsFilePath = path.resolve(__dirname, 'src/assets/json/claims.json');
 
 // Endpoint para obtener las reclamaciones
 app.get('/claims', (req: Request, res: Response) => {
@@ -48,24 +48,20 @@ app.post('/claims', (req: Request, res: Response) => {
 // Ruta para manejar el inicio de sesión
 app.post('/api/login', (req: Request, res: Response) => {
     const { username, password } = req.body;
-    // Leemos el archivo users.json
+
     fs.readFile(usersFileJSON, 'utf8', (err, data) => {
         if (err) {
             console.error('Error al leer el archivo users.json:', err);
             return res.status(500).json({ error: 'Error interno del servidor' });
         }
         try {
-            const users = JSON.parse(data);
-            // Verificamos si el nombre de usuario y la contraseña se encuentran en el JSON de usuarios
+            const parsedData = JSON.parse(data);
+            const users = parsedData.users; // Aquí accedes a la propiedad 'users'
             const user = users.find((user: any) => user.username === username && user.password === password);
-            if (user) { // Usuario autenticado correctamente
-            // Almacenar el rol del usuario en el almacenamiento de sesión
-            //req.session.role = user.role;
-                // Enviar una respuesta con el nombre de usuario y el rol
+            if (user) {
                 res.json({ username: user.username, role: user.role });
             } else {
-            // Credenciales incorrectas
-            res.status(401).json({ error: 'Credenciales incorrectas' });
+                res.status(401).json({ error: 'Credenciales incorrectas' });
             }
         } catch (error) {
             console.error('Error al analizar el archivo JSON:', error);
@@ -73,6 +69,7 @@ app.post('/api/login', (req: Request, res: Response) => {
         }
     });
 });
+
 
 // Directorio donde se encuentran los archivos estáticos de Angular
 const angularDistPath = path.resolve(__dirname, '../dist/blueprint-project/browser');
